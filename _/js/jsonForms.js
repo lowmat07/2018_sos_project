6655fc4c146a6f1efeb4f8ccff07a3b57d0efa88
Lowdermilk <lowmat07@ada.evergreen.edu>
2018-05-07 17:19:50 -0700

    local-storage successfully added

---
+++ _/js/jsonForms.js
@@ -0,0 +1,14 @@
+// Namespace our app
+var app = app || {};
+
+app.jsonForms = [
+  {
+    "form": "what's up",
+    "inst": [
+      "hello",
+      "what's wrong",
+      "what are you doing",
+      "how are you"
+    ]
+  }
+];
---
+++ working/_/sampleData.js
@@ -0,0 +1,133 @@
+var expressions = [
+  {
+    "fid": 0000000001,
+    "form": "we'll just have to see how it goes",
+    "interpretations": [
+      {
+        "aspect":"we must wait indeterminate time to tell success or failure of venture",
+        "tags":["unambiguous"]
+      },
+      {
+        "aspect":"as long as no one messes anything up, things will go well",
+        "tags":["mild pessimism","pessimistic"] 
+      }
+    ],
+    "tags": [
+      "optimistic",
+      "pessimistic",
+      "generic"
+    ],
+    "alternates": [
+      "we'll see how it turns out",
+      "let's see what the Fates have in store",
+      "as God wills",
+      "as God decides"
+    ]
+  },
+  {
+    "fid": 0000000002,
+    "form": "we'll see how it turns out",
+    "interpretations": [
+      {
+        "aspect":"we must wait indeterminate time to tell success or failure of venture",
+        "tags":["unambiguous"]
+      },
+      {
+        "aspect":"as long as no one messes anything up, things will go well",
+        "tags":["mild pessimism","pessimistic"]
+      }
+    ],
+    "tags": [
+      "optimistic",
+      "pessimistic",
+      "generic"
+    ],
+    "alternates": [
+      "we'll just have to see how it goes",
+      "let's see what the Fates have in store",
+      "as God wills",
+      "as the gods decide"
+    ]
+  },
+  {
+    "fid": 0000000003,
+    "form": "let's see what the Fates have in store",
+    "interpretations": [
+      {
+        "aspect":"we must wait indeterminate time to tell success or failure of venture",
+        "tags":["unambiguous"]
+      },
+      {
+        "aspect":"as long as no one messes anything up, things will go well",
+        "tags":["mild pessimism","pessimistic"]
+      }
+    ],
+    "tags": [
+      "optimistic",
+      "pessimistic",
+      "religious",
+      "Pagan",
+      "Greco-Roman"
+    ],
+    "alternates": [
+      "we'll just have to see how it goes",
+      "we'll see how it turns out",
+      "as God wills",
+      "as the gods decide"
+    ]
+  },
+  {
+    "fid": 0000000004,
+    "form": "as God wills",
+    "interpretations": [
+      {
+        "aspect":"we must wait indeterminate time to tell success or failure of venture",
+        "tags":["unambiguous"]
+      },
+      {
+        "aspect":"as long as no one messes anything up, things will go well",
+        "tags":["mild pessimism","pessimistic"]
+      }
+    ],
+    "tags": [
+      "optimistic",
+      "pessimistic",
+      "religious",
+      "Christian",
+      "Jewish"
+    ],
+    "alternates": [
+      "we'll just have to see how it goes",
+      "we'll see how it turns out",
+      "let's see what the Fates have in store",
+      "as the gods decide"
+    ]
+  },
+  {
+    "fid": 0000000005,
+    "form": "as the gods decide",
+    "interpretations": [
+      {
+        "aspect":"we must wait indeterminate time to tell success or failure of venture",
+        "tags":["unambiguous"]
+      },
+      {
+        "aspect":"as long as no one messes anything up, things will go well",
+        "tags":["mild pessimism","pessimistic"]
+      }
+    ],
+    "tags": [
+      "optimistic",
+      "pessimistic",
+      "Religious",
+      "Hindu",
+      "Buddhist"
+    ],
+    "alternates": [
+      "we'll just have to see how it goes",
+      "we'll see how it turns out",
+      "let's see what the Fates have in store",
+      "as God wills"
+    ]
+  }
+];
